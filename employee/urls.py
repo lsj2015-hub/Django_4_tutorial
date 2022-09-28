310ec935e934e5ad124b905c214451757b5f144e
lsj2015-hub <lsj2015@hotmail.com>
2022-09-28 20:44:59 +0900

    Django Tutorial finished!

--- employee/urls.py
+++ employee/urls.py
@@ -1,6 +1,17 @@
+from pipes import Template
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-  path('', views.index, name='index')
+  # employee
+  path('employee/', views.index, name='employee'),
+  path('create/', views.create, name='create'),
+  path('create/create_data/', views.create_data, name='create_data'),
+  path('delete/<int:id>/', views.delete, name='delete'),
+  path('update/<int:id>/', views.update, name='update'),
+  path('update/update_data/<int:id>/', views.update_data, name='update_data'),
+  # blog
+  path('', views.blog, name='blog'),
+  path('detail/<int:id>/', views.detail_page, name='detail'),
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,6 +1,66 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from .models import Employee, BlogPosts
 # Create your views here.
 
 def index(request):
-  return HttpResponse('<h1>Hello from Empolyee</h1>')
+  my_employees = Employee.objects.all().order_by('name')
+  template = loader.get_template('employee/index.html')
+  context = {
+    'my_employees': my_employees
+  }
+  return HttpResponse(template.render(context, request))
+
+def create(request):
+  template = loader.get_template('employee/create_page.html')
+  return HttpResponse(template.render({}, request))
+
+def create_data(request):
+  name = request.POST['name']
+  title = request.POST['title']
+  new_employee = Employee(name=name, title=title)
+  new_employee.save()
+  return HttpResponseRedirect(reverse('employee'))
+
+def delete(request, id):
+  delete_employee = Employee.objects.get(id=id)
+  delete_employee.delete()
+  return HttpResponseRedirect(reverse('employee'))
+
+def update(request, id):
+  update_employee = Employee.objects.get(id=id)
+  template = loader.get_template('employee/update_page.html')
+  context = {
+    'Employee': update_employee
+  }
+  return HttpResponse(template.render(context, request))
+ 
+def update_data(request, id):
+  update_name = request.POST['name']
+  update_title = request.POST['title']
+  update_employee = Employee.objects.get(id=id)
+  update_employee.name = update_name
+  update_employee.title = update_title
+  update_employee.save()
+  return HttpResponseRedirect(reverse('employee'))
+
+def blog(request):
+  posts = BlogPosts.objects.all()
+  featured_posts = BlogPosts.objects.filter(featured=True)
+  template = loader.get_template('employee/blog.html')
+  context = {
+    'posts': posts,
+    'featured_posts': featured_posts
+  }
+
+  return HttpResponse(template.render(context, request))
+
+def detail_page(request, id):
+  detail_post = BlogPosts.objects.get(id=id)
+  template = loader.get_template('employee/detail_page.html')
+  context = {
+    'post': detail_post
+  }
+  return HttpResponse(template.render(context, request))
